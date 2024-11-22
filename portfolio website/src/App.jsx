8acb5d89f4f599d76ea2df227302ddfc3b596be9
Huzaifa Jambagi <huzaifajambagi10@gmail.com>
2024-11-22 19:05:38 +0530

    portfolio

---
+++ portfolio website/src/App.jsx
@@ -0,0 +1,18 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { useState } from 'react';
+import './App.css';
+import Banner from './components/Banner';
+import NavBar from './components/NavBar';
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+  <div className='App'>
+ <NavBar/>
+ <Banner/>
+  </div>
+  )
+}
+
+export default App
